b44a4e024fc79b3a6a96fa5b9ac2d33961555cc4
akbhuker <akbhuker49@gmail.com>
2022-07-18 20:28:27 +0530

    Version 2

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmVpS1_wVMvChs2B8PaUoy7-zhSRQdekw",
+  authDomain: "crypto-stalker.firebaseapp.com",
+  projectId: "crypto-stalker",
+  storageBucket: "crypto-stalker.appspot.com",
+  messagingSenderId: "520589585426",
+  appId: "1:520589585426:web:58255eaa7518b853e73ccb"
+};
+
+export default firebaseConfig;
